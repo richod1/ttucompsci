90c4102e09c098d0cea57f93e9b8f4316ffad45e
richod1 <frimpsup@gmail.com>
2024-02-11 12:10:58 +0000

    s3-server added

--- app/lib/s3.server.ts
+++ app/lib/s3.server.ts
@@ -1,12 +1,41 @@
 import {S3Client} from "@aws-sdk/client-s3"
 import {Upload} from "@aws-sdk/lib-storage"
 import aws from "aws-sdk"
-import {Redable} from "stream"
+import {Readable} from "stream"
 
-
+const endpoint=new aws.Endpoint("https://ttucompsci.eu-central-1.linodeobjects.com/")
 
 // init object upload
+// @ts-ignore
+const s3 = new S3Client({
+	endpoint,
+	credentials: {
+		accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+	},
+	region: "eu-central-1",
+});
 
+async function upload(
+	stream: AsyncIterable<Uint8Array> | Buffer,
+	filename: string,
+	contentType?: string,
+) {
+	return new Upload({
+		client: s3,
+		leavePartsOnError: false,
+		params: {
+			ACL: "public-read",
+			Bucket: "ttucompsci",
+			Key: filename,
+			ContentType: contentType,
+			CacheControl: "max-age=31536000",
+			Body: Readable.from(stream),
+		},
+	}).done();
+}
+
+export { upload };
 
 
 
