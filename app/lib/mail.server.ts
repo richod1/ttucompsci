647a121cddc7949b5d6d461486fa92e1f83e0ec9
richod1 <frimpsup@gmail.com>
2024-02-19 13:52:57 +0000

    fix debug

--- app/lib/mail.server.ts
+++ app/lib/mail.server.ts
@@ -1,7 +1,13 @@
-import {Resend} from "resend"
+// import {Resend} from "resend"
+import Mailgun from "mailgun.js"
+import FormData from "form-data";
 
-const resend=new Resend(process.env.RESEND_API_KEY)
-const send=resend.emails.send.bind(resend.emails);
-console.log(send)
+
+const mailgun=new Mailgun(FormData);
+const send=mailgun.client({username:'api',key:process.env.MAILGUN_API || "mailapi"})
+
+// const resend=new Resend(process.env.RESEND_API_KEY)
+// const send=resend.emails.send.bind(resend.emails);
+// console.log(send)
 
 export {send}
